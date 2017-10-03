b2e345a696697a729005e0c5c8db58481824f0e6
shreya4496 <shreyagupta4496@gmail.com>
2017-10-03 09:54:04 +0530

    Framework added

---
+++ screenshare/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ScreenshareConfig(AppConfig):
+    name = 'screenshare'
---
+++ trend_promotion/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TrendPromotionConfig(AppConfig):
+    name = 'trend_promotion'
